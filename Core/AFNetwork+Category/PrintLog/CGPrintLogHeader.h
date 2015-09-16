14513c465731513dafc60065e823f55d24bde04b
dj <410500921@qq.com>
2015-09-16 09:42:24 +0800

    打印日志页加了点注释，区分明显点

--- Core/AFNetwork+Category/PrintLog/CGPrintLogHeader.h
+++ Core/AFNetwork+Category/PrintLog/CGPrintLogHeader.h
@@ -9,7 +9,7 @@
 #ifndef QuickAskCommunity_CGPrintLogHeader_h
 #define QuickAskCommunity_CGPrintLogHeader_h
 
-#ifdef DEBUG
+#ifdef DEBUG //---------if
 
 #define CGLog(format, ...) do {                                                     \
         fprintf(stderr, "<%s : %d> %s\n",                                           \
@@ -23,13 +23,13 @@
 #define CGDebugAssert(condition, desc, ...)     NSAssert(condition, (desc), ##__VA_ARGS__)
 #define CGDebugAssert1(condition, desc, arg1)   CGDebugAssert(condition, (desc), (arg1))
 
-#else
+#else   //-----------else
 
 #define CGLog(format, ...)
 
 #define CGDebugAssert(condition, desc, ...)
 #define CGDebugAssert1(condition, desc, arg1)
 
-#endif
+#endif  //-----------end
 
 #endif
