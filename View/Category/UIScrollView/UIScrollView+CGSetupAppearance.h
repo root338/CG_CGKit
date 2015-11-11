380216c97de60e80ce1613239e10405f60b10bf9
dj <410500921@qq.com>
2015-11-11 17:53:12 +0800

    增加UIScrollView扩展，没加什么方法

---
+++ View/Category/UIScrollView/UIScrollView+CGSetupAppearance.h
@@ -0,0 +1,14 @@
+//
+//  UIScrollView+CGSetupAppearance.h
+//  QuickAskCommunity
+//
+//  Created by DY on 15/11/11.
+//  Copyright © 2015年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (CGSetupAppearance)
+
+- (void)cg_setupScrollIndicatorWithShow:(BOOL)isShow;
+@end
---
+++ View/Category/UIScrollView/UIScrollView+CGSetupAppearance.m
@@ -0,0 +1,18 @@
+//
+//  UIScrollView+CGSetupAppearance.m
+//  QuickAskCommunity
+//
+//  Created by DY on 15/11/11.
+//  Copyright © 2015年 ym. All rights reserved.
+//
+
+#import "UIScrollView+CGSetupAppearance.h"
+
+@implementation UIScrollView (CGSetupAppearance)
+
+- (void)cg_setupScrollIndicatorWithShow:(BOOL)isShow
+{
+    self.showsHorizontalScrollIndicator = isShow;
+    self.showsHorizontalScrollIndicator = isShow;
+}
+@end
