d3c4ee636eb718decbc0d1b52ab3b7aa10013123
dj <410500921@qq.com>
2016-01-25 10:25:43 +0800

    增加视图移动扩展（没加方法）

---
+++ Anmation/MoveView/UIView+CGMoveView.h
@@ -0,0 +1,15 @@
+//
+//  UIView+CGMoveView.h
+//  TestCG_CGKit
+//
+//  Created by DY on 16/1/5.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CGMoveView)
+
+
+
+@end
---
+++ Anmation/MoveView/UIView+CGMoveView.m
@@ -0,0 +1,13 @@
+//
+//  UIView+CGMoveView.m
+//  TestCG_CGKit
+//
+//  Created by DY on 16/1/5.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "UIView+CGMoveView.h"
+
+@implementation UIView (CGMoveView)
+
+@end
