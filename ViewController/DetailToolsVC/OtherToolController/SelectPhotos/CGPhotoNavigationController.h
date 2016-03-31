8004adbcbf4f3761b37295e91935fb70681d038c
dj <410500921@qq.com>
2016-03-31 09:38:27 +0800

    添加添加图片导航栏类

---
+++ ViewController/DetailToolsVC/OtherToolController/SelectPhotos/CGPhotoNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  CGPhotoNavigationController.h
+//  TestCG_CGKit
+//
+//  Created by DY on 16/3/9.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "CGNavigationController.h"
+
+@interface CGPhotoNavigationController : CGNavigationController
+
+@end
---
+++ ViewController/DetailToolsVC/OtherToolController/SelectPhotos/CGPhotoNavigationController.m
@@ -0,0 +1,13 @@
+//
+//  CGPhotoNavigationController.m
+//  TestCG_CGKit
+//
+//  Created by DY on 16/3/9.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "CGPhotoNavigationController.h"
+
+@implementation CGPhotoNavigationController
+
+@end
