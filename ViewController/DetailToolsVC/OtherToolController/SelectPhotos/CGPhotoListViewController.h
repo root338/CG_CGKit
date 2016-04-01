5ce7e0fd3924314539e71f6e39124099f2765ed6
dj <410500921@qq.com>
2016-04-01 10:59:42 +0800

    添加照片列表

---
+++ ViewController/DetailToolsVC/OtherToolController/SelectPhotos/CGPhotoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  CGPhotoListViewController.h
+//  TestCG_CGKit
+//
+//  Created by DY on 16/3/31.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "CGBaseViewController.h"
+
+@interface CGPhotoListViewController : CGBaseViewController
+
+@end
---
+++ ViewController/DetailToolsVC/OtherToolController/SelectPhotos/CGPhotoListViewController.m
@@ -0,0 +1,13 @@
+//
+//  CGPhotoListViewController.m
+//  TestCG_CGKit
+//
+//  Created by DY on 16/3/31.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "CGPhotoListViewController.h"
+
+@implementation CGPhotoListViewController
+
+@end
