32e5cf81313ee7ed50fa538ff9756be6578f416f
dj <410500921@qq.com>
2016-06-16 10:39:04 +0800

    增加UINavigationController+CGPushViewController自定义push视图控制器

---
+++ ViewController/Category/UINavigationController+CGPushViewController.h
@@ -0,0 +1,40 @@
+//
+//  UINavigationController+CGPushViewController.h
+//  QuickAskCommunity
+//
+//  Created by DY on 16/6/15.
+//  Copyright © 2016年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/** 弹出视图控制器的扩展 */
+@interface UINavigationController (CGPushViewController)
+
+/** 默认push视图控制器隐藏动画，默认值为 NO */
+@property (nonatomic, assign) BOOL defaultPushHideAnimatied;
+
+/** push视图控制器 */
+- (void)cg_pushViewController:(UIViewController *)viewController;
+
+/** push视图控制器，删除最上层的视图控制器 */
+- (void)cg_pushRemoveLastVCWithNewViewController:(UIViewController *)viewController;
+/** push视图控制器，删除最上层的视图控制器 */
+- (void)cg_pushRemoveLastVCWithNewViewController:(UIViewController *)viewController animated:(BOOL)animated;
+
+/** push视图控制器，且删除指定视图控制器 */
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllers:(NSArray<UIViewController *> *)viewControllers;
+/** push视图控制器，且删除指定视图控制器 */
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllers:(NSArray<UIViewController *> *)viewControllers animated:(BOOL)animated;
+
+/** push视图控制器，且删除指定类下的所有视图控制器 */
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllerClass:(NSArray<Class> *)viewControllerClass;
+/** push视图控制器，且删除指定类下的所有视图控制器 */
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllerClass:(NSArray<Class> *)viewControllerClass animated:(BOOL)animated;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
---
+++ ViewController/Category/UINavigationController+CGPushViewController.m
@@ -0,0 +1,78 @@
+//
+//  UINavigationController+CGPushViewController.m
+//  QuickAskCommunity
+//
+//  Created by DY on 16/6/15.
+//  Copyright © 2016年 ym. All rights reserved.
+//
+
+#import "UINavigationController+CGPushViewController.h"
+
+#import <objc/runtime.h>
+
+@implementation UINavigationController (CGPushViewController)
+
+- (void)cg_pushViewController:(UIViewController *)viewController
+{
+    [self.navigationController pushViewController:viewController animated:!self.defaultPushHideAnimatied];
+}
+
+- (void)cg_pushRemoveLastVCWithNewViewController:(UIViewController *)viewController
+{
+    [self cg_pushRemoveLastVCWithNewViewController:viewController animated:!self.defaultPushHideAnimatied];
+}
+
+- (void)cg_pushRemoveLastVCWithNewViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    NSMutableArray *newViewControllers  = [NSMutableArray arrayWithArray:self.viewControllers];
+    [newViewControllers removeLastObject];
+    [self setViewControllers:newViewControllers animated:animated];
+}
+
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllers:(NSArray<UIViewController *> *)viewControllers
+{
+    [self cg_pushViewController:viewController removeViewControllers:viewControllers animated:!self.defaultPushHideAnimatied];
+}
+
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllers:(NSArray<UIViewController *> *)viewControllers animated:(BOOL)animated
+{
+    NSMutableArray *newViewControllers  = [NSMutableArray arrayWithCapacity:MAX(1, self.viewControllers.count - viewControllers.count)];
+    [self.viewControllers enumerateObjectsUsingBlock:^(__kindof UIViewController * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (![viewControllers containsObject:obj]) {
+            [newViewControllers addObject:viewControllers];
+        }
+    }];
+    [self setViewControllers:newViewControllers animated:animated];
+}
+
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllerClass:(NSArray<Class> *)viewControllerClass
+{
+    [self cg_pushViewController:viewController removeViewControllerClass:viewControllerClass animated:!self.defaultPushHideAnimatied];
+}
+
+- (void)cg_pushViewController:(UIViewController *)viewController removeViewControllerClass:(NSArray<Class> *)viewControllerClass animated:(BOOL)animated
+{
+    NSMutableArray *newViewControllers  = [NSMutableArray arrayWithCapacity:MAX(1, self.viewControllers.count - viewControllerClass.count)];
+    [self.viewControllers enumerateObjectsUsingBlock:^(__kindof UIViewController * _Nonnull viewController, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        [viewControllerClass enumerateObjectsUsingBlock:^(Class  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if (![viewController isMemberOfClass:[obj class]]) {
+                [newViewControllers addObject:viewController];
+            }
+        }];
+    }];
+    [self setViewControllers:newViewControllers animated:animated];
+}
+
+#pragma mark - 设置属性 
+
+- (void)setDefaultPushHideAnimatied:(BOOL)animatied
+{
+    objc_setAssociatedObject(self, @selector(defaultPushHideAnimatied), @(animatied), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (BOOL)defaultPushHideAnimatied
+{
+    return [objc_getAssociatedObject(self, _cmd) boolValue];
+}
+@end
