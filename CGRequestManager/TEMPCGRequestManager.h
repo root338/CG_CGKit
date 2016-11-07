8dc275cdba9a1ce4da70a1d156b214cd01337ae8
DY <dy@yuemei.com>
2016-11-07 18:19:19 +0800

    追加类注释的解释

--- CGRequestManager/TEMPCGRequestManager.h
+++ CGRequestManager/TEMPCGRequestManager.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 ym. All rights reserved.
 //
 
-//与CG_CGKit/Foundation/NeedStandByLibraryClass/AFNetwork+Category/Manager/CGRequestManager.h文件重命名 
+//与CG_CGKit/Foundation/NeedStandByLibraryClass/AFNetwork+Category/Manager/CGRequestManager.h文件重命名
 //#import <Foundation/Foundation.h>
 //
 //@interface CGRequestManager : NSObject
