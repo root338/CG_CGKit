19efb1916520b6d1976d4deddc12537d5beeb8b9
DY <dy@yuemei.com>
2016-11-18 18:20:43 +0800

    增加选择框选择视图

---
+++ View/CustomView/View/SelectorPromptView/CGSelectorView.h
@@ -0,0 +1,158 @@
+//
+//  CGBaseAlertContentView.h
+//  QuickAskCommunity
+//
+//  Created by DY on 2016/11/18.
+//  Copyright © 2016年 ym. All rights reserved.
+//
+
+#import "CGBaseView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class CGSelectorView;
+
+typedef void (^CGSelectorAnimationBlock) (void);
+typedef void (^CGSelectorAnimationCompletionBlock) (BOOL finished);
+
+/** content view 的动画方式 */
+typedef NS_ENUM(NSInteger, CGSelectorContentViewAnimationType) {
+    /** 由底部向上 */
+    CGSelectorContentViewAnimationTypeBottomToTop,
+    /** 由顶部向下 */
+    CGSelectorContentViewAnimationTypeTopToBottom,
+    /** 由左向右 */
+    CGSelectorContentViewAnimationTypeLeftToRight,
+    /** 由右向左 */
+    CGSelectorContentViewAnimationTypeRightToLeft,
+};
+
+/** 动画的状态 */
+typedef NS_ENUM(NSInteger, CGViewAnimationStatus) {
+    /** 未知 */
+    CGViewAnimationStatusUnknowe    = -1,
+    /** 静止 */
+    CGViewAnimationStatusStill,
+    /** 动画执行中 */
+    CGViewAnimationStatusAniamtion,
+};
+
+/** 视图的状态 */
+typedef NS_ENUM(NSInteger, CGViewStatus) {
+    /** 未知 */
+//    CGViewStatusUnknowe     = -1,
+    /** 显示 */
+    CGViewStatusShow,
+    /** 由显示过滤到隐藏状态，一般为动画执行过程 */
+    CGViewStatusShowToHide,
+    /** 隐藏 */
+    CGViewStatusHide,
+    /** 由隐藏过滤到显示状态，一般为动画执行过程 */
+    CGViewStatusHideToShow,
+};
+
+/** 视图动画的效果 */
+typedef NS_ENUM(NSInteger, CGViewAnimationsStyle) {
+    /** 默认效果 */
+    CGViewAnimationsStyleDefalut,
+    /** 弹簧效果 */
+    CGViewAnimationsStyleSpringEffect,
+    
+    /** 自定义动画效果，@warning 所有设置项全部失效，需要用户自定义实现 */
+//    CGViewAnimationsStyleCustom,
+};
+
+/**
+ 弹层选择视图
+ @param 内容选择器应该添加到contentView中
+ @param 默认背景色 黑色透明度 50%
+ */
+@interface CGSelectorView : CGBaseView
+
+#pragma mark - 视图设置
+/** 选择的内容视图 */
+@property (nonatomic, strong, readonly) UIView *contentView;
+
+/** 选择视图大小是否等于父视图的bounds属性, 默认为 YES */
+@property (nonatomic, assign) BOOL frameEqualSuperviewBounds;
+
+/** 隐藏contentView后移除父视图, 默认YES */
+@property (nonatomic, assign) BOOL didHideContentViewRemoveSuperview;
+/** 选择视图的父视图 */
+@property (nullable, nonatomic, weak) UIView *targetSuperview;
+/** 设置选择视图的父视图block */
+@property (nullable, nonatomic, copy) UIView * (^setupSuperviewBlock) (void);
+
+/** 
+ 关闭背景图按钮功能 默认NO
+ @param 触发- (void)hideContentView方法 
+ */
+@property (nonatomic, assign) BOOL disableBackgroundButtonTools;
+
+/** 
+ 当显示选择视图时关闭导航栏的回退手势, 默认YES
+ 隐藏时自动设置为之前的值
+ */
+@property (nonatomic, assign) BOOL disableInteractivePopGestureRecognizerForNavigationController;
+
+/** 关闭动画执行中的用户交互, 默认 NO */
+@property (nonatomic, assign) BOOL disableAnimationUserInteraction;
+/** 动画的状态 */
+@property (nonatomic, assign, readonly) CGViewAnimationStatus animationStatus;
+/** contentView 的状态 */
+@property (nonatomic, assign, readonly) CGViewStatus contentViewStatus;
+
+#pragma mark - 背景设置
+/** 动画执行选择视图背景色的改变，默认 YES */
+@property (nonatomic, assign) BOOL animationChangeBackgroundColor;
+/** 选择视图未显示时的颜色，默认 nil */
+@property (nullable, nonatomic, strong) UIColor *fromBackgroundColor;
+/** 选择视图显示时的颜色，默认 [UIColor colorWithWhite:0 alpha:0.5] */
+@property (nullable, nonatomic, strong) UIColor *toBackgroundColor;
+
+#pragma mark - 动画方式设置
+/** 选择视图的动画效果类型, 默认 CGViewAnimationsStyleDefalut */
+@property (nonatomic, assign) CGViewAnimationsStyle animationsStyle;
+
+/** 选择内容视图的动画方式， 默认 CGSelectorContentViewAnimationTypeBottomToTop */
+@property (nonatomic, assign) CGSelectorContentViewAnimationType contentViewAnimationType;
+/** 动画执行时间，默认0.3 */
+@property (nonatomic, assign) NSTimeInterval duration;
+/** 选择视图显示时动画延迟执行时间, 默认0 */
+@property (nonatomic, assign) NSTimeInterval delayForContentViewShow;
+/** 选择视图隐藏时动画延迟执行时间, 默认0 */
+@property (nonatomic, assign) NSTimeInterval delayForContentViewHide;
+/** 选择视图显示时动画的实现方式， 默认 UIViewAnimationOptionCurveLinear */
+@property (nonatomic, assign) UIViewAnimationOptions contentViewShowAnimationOptions;
+/** 选择视图隐藏时动画的实现方式, 默认 UIViewAnimationOptionCurveLinear */
+@property (nonatomic, assign) UIViewAnimationOptions contentViewHideAnimationOptions;
+
+#pragma mark - 动画时的状态设置
+/** 选择器在显示选择视图动画前的设置 */
+@property (nullable, nonatomic, copy) CGSelectorAnimationBlock selectorWillShowAnimationsBlock;
+/** 选择器在显示选择视图动画时的设置 */
+@property (nullable, nonatomic, copy) CGSelectorAnimationBlock selectorSetupShowAnimationsBlock;
+/** 选择器在隐藏选择视图动画前的设置 */
+@property (nullable, nonatomic, copy) CGSelectorAnimationBlock selectorWillHideAnimationsBlock;
+/** 选择器在隐藏选择视图动画时的设置 */
+@property (nullable, nonatomic, copy) CGSelectorAnimationBlock selectorSetupHideAnimationsBlock;
+
+/** 选择器在显示选择视图动画完成后调用 */
+@property (nullable, nonatomic, copy) CGSelectorAnimationCompletionBlock selectorShowAnimationsCompletion;
+/** 选择器在隐藏选择视图动画完成后调用 */
+@property (nullable, nonatomic, copy) CGSelectorAnimationCompletionBlock selectorHideAnimationsCompletion;
+
+#pragma mark - 弹簧效果下的属性值
+/** 0.0 ~ 1.0 阻尼比例，数值越小「弹簧」的振动效果越明显, 默认 0.7 */
+@property (nonatomic, assign) CGFloat springDampingRatio;
+/** 初始的速度，数值越大一开始移动越快, 默认 3.0 */
+@property (nonatomic, assign) CGFloat initialSpringVelocity;
+
+#pragma mark - 动画执行方法
+
+- (void)showContentView;
+- (void)hideContentView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ View/CustomView/View/SelectorPromptView/CGSelectorView.m
@@ -0,0 +1,397 @@
+//
+//  CGBaseAlertContentView.m
+//  QuickAskCommunity
+//
+//  Created by DY on 2016/11/18.
+//  Copyright © 2016年 ym. All rights reserved.
+//
+
+#import "CGSelectorView.h"
+
+#import "UIView+CGSearchView.h"
+#import "UIView+CGSetupFrame.h"
+
+#import "CGPrintLogHeader.h"
+
+@interface CGSelectorView ()
+{
+    UIButton *cancelButton;
+    //记录导航栏回退手势的状态
+    BOOL interactivePopGestureRecognizerEnabledForNavigationController;
+}
+
+@property (nonatomic, strong, readwrite) UIView *contentView;
+
+/** 动画的状态 */
+@property (nonatomic, assign, readwrite) CGViewAnimationStatus animationStatus;
+/** contentView 的状态 */
+@property (nonatomic, assign, readwrite) CGViewStatus contentViewStatus;
+@end
+
+@implementation CGSelectorView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        _contentView    = [[UIView alloc] init];
+        
+        _disableBackgroundButtonTools       = NO;
+        _didHideContentViewRemoveSuperview  = YES;
+        _frameEqualSuperviewBounds          = YES;
+        _animationChangeBackgroundColor     = YES;
+        _animationsStyle                    = CGViewAnimationsStyleDefalut;
+        _contentViewAnimationType           = CGSelectorContentViewAnimationTypeBottomToTop;
+        _fromBackgroundColor                = nil;
+        _toBackgroundColor                  = [UIColor colorWithWhite:0 alpha:0.5];
+        
+        _duration                           = 0.3;
+        _delayForContentViewShow            = 0;
+        _delayForContentViewHide            = 0;
+        _contentViewShowAnimationOptions    = UIViewAnimationOptionCurveLinear;
+        _contentViewHideAnimationOptions    = UIViewAnimationOptionCurveLinear;
+        
+        _disableAnimationUserInteraction    = NO;
+        _animationStatus    = CGViewAnimationStatusStill;
+        _contentViewStatus  = CGViewStatusHide;
+        _disableInteractivePopGestureRecognizerForNavigationController  = YES;
+        
+        _springDampingRatio                 = 0.7;
+        _initialSpringVelocity              = 3.0;
+    }
+    return self;
+}
+
+- (void)willMoveToSuperview:(UIView *)newSuperview
+{
+    [super willMoveToSuperview:newSuperview];
+    if (newSuperview) {
+        self.contentView.superview ?: [self addSubview:_contentView];
+    }
+}
+
+- (void)showContentView
+{
+    [self showContentViewWithAnimatinType:self.contentViewAnimationType];
+}
+
+- (void)hideContentView
+{
+    [self hideContentViewWithAnimationType:self.contentViewAnimationType];
+}
+
+- (void)showContentViewWithAnimatinType:(CGSelectorContentViewAnimationType)animationType
+{
+    if (self.contentViewStatus == CGViewStatusShow || self.contentViewStatus == CGViewStatusHideToShow) {
+        return;
+    }
+    
+    [self setupSelfLayout];
+    
+    if (self.disableInteractivePopGestureRecognizerForNavigationController) {
+        [self setupNavigationControllerPopGestureRecognizerWithisShowContentViewAnimation:YES];
+    }
+    
+    [self setupBackgroundColorWithIsShow:YES animations:NO];
+    
+    self.contentView.frame  = [self setupContentViewFrameWithAnimationType:animationType showContentView:NO];
+    
+    if (self.selectorWillShowAnimationsBlock) {
+        self.selectorWillShowAnimationsBlock();
+    }
+    
+    __weak __block typeof(self) weakself = self;
+    CGSelectorAnimationBlock animations = ^{
+        [weakself setupShowContentViewAnimationsWithType:animationType];
+    };
+    CGSelectorAnimationCompletionBlock completion = ^(BOOL finished){
+        [weakself setupShowContentViewAnimationsCompletions:finished];
+    };
+    
+    if (self.animationsStyle == CGViewAnimationsStyleDefalut) {
+        [UIView animateWithDuration:self.duration delay:self.delayForContentViewShow options:self.contentViewShowAnimationOptions animations:animations completion:completion];
+    }else if (self.animationsStyle == CGViewAnimationsStyleSpringEffect) {
+        [UIView animateWithDuration:self.duration delay:self.delayForContentViewShow usingSpringWithDamping:self.springDampingRatio initialSpringVelocity:self.initialSpringVelocity options:self.contentViewShowAnimationOptions animations:animations completion:completion];
+    }
+}
+
+//显示选择视图动画时的属性变化设置
+- (void)setupShowContentViewAnimationsWithType:(CGSelectorContentViewAnimationType)animationType
+{
+    [self setupBackgroundColorWithIsShow:YES animations:YES];
+    self.contentView.frame  = [self setupContentViewFrameWithAnimationType:animationType showContentView:YES];
+    
+    if (self.selectorSetupShowAnimationsBlock) {
+        self.selectorSetupShowAnimationsBlock();
+    }
+    
+    self.contentViewStatus  = CGViewStatusHideToShow;
+    self.animationStatus    = CGViewAnimationStatusAniamtion;
+}
+//显示选择视图动画完成时的设置
+- (void)setupShowContentViewAnimationsCompletions:(BOOL)finished
+{
+    if (self.selectorShowAnimationsCompletion) {
+        self.selectorShowAnimationsCompletion(finished);
+    }
+    
+    self.contentViewStatus  = CGViewStatusShow;
+    self.animationStatus    = CGViewAnimationStatusStill;
+}
+
+- (void)hideContentViewWithAnimationType:(CGSelectorContentViewAnimationType)animationType
+{
+    if (self.contentViewStatus == CGViewStatusShowToHide || self.contentViewStatus == CGViewStatusHide) {
+        return;
+    }
+    
+    if (self.disableInteractivePopGestureRecognizerForNavigationController) {
+        [self setupNavigationControllerPopGestureRecognizerWithisShowContentViewAnimation:NO];
+    }
+    
+    [self setupBackgroundColorWithIsShow:NO animations:NO];
+    
+    if (self.selectorWillHideAnimationsBlock) {
+        self.selectorWillHideAnimationsBlock();
+    }
+    
+    __weak __block typeof(self) weakself = self;
+    CGSelectorAnimationBlock animations = ^{
+        [weakself setupHideContentViewAnimationsWithType:animationType];
+    };
+    CGSelectorAnimationCompletionBlock completion = ^(BOOL finished){
+        [weakself setupHideContentViewAnimationsCompletions:finished];
+    };
+    
+    if (self.animationsStyle == CGViewAnimationsStyleDefalut) {
+        [UIView animateWithDuration:self.duration delay:self.delayForContentViewHide options:self.contentViewHideAnimationOptions animations:animations completion:completion];
+    }else if (self.animationsStyle == CGViewAnimationsStyleSpringEffect) {
+        [UIView animateWithDuration:self.duration delay:self.delayForContentViewHide usingSpringWithDamping:self.springDampingRatio initialSpringVelocity:self.initialSpringVelocity options:self.contentViewHideAnimationOptions animations:animations completion:completion];
+    }
+}
+
+//显示选择视图动画时的属性变化设置
+- (void)setupHideContentViewAnimationsWithType:(CGSelectorContentViewAnimationType)animationType
+{
+    [self setupBackgroundColorWithIsShow:NO animations:YES];
+    self.contentView.frame  = [self setupContentViewFrameWithAnimationType:animationType showContentView:NO];
+    
+    if (self.selectorSetupHideAnimationsBlock) {
+        self.selectorSetupHideAnimationsBlock();
+    }
+    
+    self.contentViewStatus  = CGViewStatusShowToHide;
+    self.animationStatus    = CGViewAnimationStatusAniamtion;
+}
+//隐藏选择视图动画完成时的设置
+- (void)setupHideContentViewAnimationsCompletions:(BOOL)finished
+{
+    if (self.selectorHideAnimationsCompletion) {
+        self.selectorHideAnimationsCompletion(finished);
+    }
+    
+    self.contentViewStatus  = CGViewStatusHide;
+    self.animationStatus    = CGViewAnimationStatusStill;
+    
+    if (self.didHideContentViewRemoveSuperview) {
+        [self removeFromSuperview];
+    }
+}
+
+/**
+ 设置contentView的显示区域
+ @warning 改方法仅修改必需的属性值，不会修改其他不相关的属性值
+
+ @param animationType 动画执行的方式
+ @param isShowContentView 是否显示contentView
+ @return 返回contentView的显示区域
+ */
+- (CGRect)setupContentViewFrameWithAnimationType:(CGSelectorContentViewAnimationType)animationType showContentView:(BOOL)isShowContentView
+{
+    CGFloat originX = 0, originY = 0;
+    
+    CGFloat width   = self.width;
+    CGFloat height  = self.height;
+    
+    CGFloat contentViewWidth = self.contentView.width;
+    CGFloat contentViewHeight = self.contentView.height;
+    
+    if (animationType == CGSelectorContentViewAnimationTypeBottomToTop || animationType == CGSelectorContentViewAnimationTypeTopToBottom) {
+        originX = self.contentView.xOrigin;
+    }else if (animationType == CGSelectorContentViewAnimationTypeRightToLeft || animationType == CGSelectorContentViewAnimationTypeLeftToRight) {
+        originY = self.contentView.yOrigin;
+    }
+    
+    switch (animationType) {
+        case CGSelectorContentViewAnimationTypeBottomToTop:
+        {
+            if (isShowContentView) {
+                originY = height - contentViewHeight;
+            }else {
+                originY = height;
+            }
+        }
+            break;
+        case CGSelectorContentViewAnimationTypeTopToBottom:
+        {
+            if (isShowContentView) {
+                originY = 0;
+            }else {
+                originY = -contentViewHeight;
+            }
+        }
+            break;
+        case CGSelectorContentViewAnimationTypeLeftToRight:
+        {
+            if (isShowContentView) {
+                originX = 0;
+            }else {
+                originX = -contentViewWidth;
+            }
+        }
+            break;
+        case CGSelectorContentViewAnimationTypeRightToLeft:
+        {
+            if (isShowContentView) {
+                originX = width - contentViewWidth;
+            }else {
+                originX = width;
+            }
+        }
+            break;
+        default:
+            break;
+    }
+    
+    return CGRectMake(originX, originY, contentViewWidth, contentViewHeight);
+}
+
+/**
+ 修改视图的背景色
+
+ @param isShow 是否是显示 contentView 时的动画
+ @param animations 是否在动画中 isShow
+ */
+- (void)setupBackgroundColorWithIsShow:(BOOL)isShow animations:(BOOL)animations
+{
+    UIColor *backgroundColor    = nil;
+    if (self.animationChangeBackgroundColor) {
+        if (animations) {
+            if (isShow) {
+                backgroundColor = self.toBackgroundColor;
+            }else {
+                backgroundColor = self.fromBackgroundColor;
+            }
+        }else {
+            if (isShow) {
+                backgroundColor = self.fromBackgroundColor;
+            }else {
+                backgroundColor = self.toBackgroundColor;
+            }
+        }
+    }else {
+        if (animations) {
+            return;
+        }else {
+            if (isShow) {
+                backgroundColor = self.toBackgroundColor;
+            }else {
+                backgroundColor = self.fromBackgroundColor;
+            }
+        }
+    }
+    self.backgroundColor    = backgroundColor;
+}
+
+#pragma mark - 设置视图
+
+- (void)setupSelfLayout
+{
+    if (self.superview == nil) {
+        UIView *targetSuperview = nil;
+        if (self.setupSuperviewBlock) {
+            targetSuperview = self.setupSuperviewBlock();
+        }else {
+            targetSuperview = self.targetSuperview;
+        }
+        CGDebugAssert(targetSuperview, @"请设置选择视图的父视图");
+        [targetSuperview addSubview:self];
+        if (self.frameEqualSuperviewBounds) {
+            self.frame  = targetSuperview.bounds;
+        }
+        
+        [self setupCancelButton];
+    }
+}
+
+- (void)setupCancelButton
+{
+    if (!self.disableBackgroundButtonTools) {
+        if (cancelButton == nil) {
+            cancelButton    = [UIButton buttonWithType:UIButtonTypeCustom];
+            cancelButton.frame  = self.bounds;
+            [cancelButton addTarget:self action:@selector(handleCancelAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+        
+        if (cancelButton.superview == nil) {
+            
+            if (self.contentView.superview == nil) {
+                [self addSubview:cancelButton];
+            }else {
+                [self insertSubview:cancelButton belowSubview:self.contentView];
+            }
+        }
+    }
+}
+
+- (void)setupNavigationControllerPopGestureRecognizerWithisShowContentViewAnimation:(BOOL)isShowContentViewAnimation
+{
+    UINavigationController *navigationController    = [self cg_searchViewControllerWithClass:[UINavigationController class]];
+    
+    BOOL enabled;
+    if (isShowContentViewAnimation) {
+        interactivePopGestureRecognizerEnabledForNavigationController   = navigationController.interactivePopGestureRecognizer.enabled;
+        enabled = NO;
+    }else {
+        enabled = interactivePopGestureRecognizerEnabledForNavigationController;
+    }
+    
+    if (navigationController.interactivePopGestureRecognizer.enabled != enabled) {
+        navigationController.interactivePopGestureRecognizer.enabled    = enabled;
+    }
+}
+
+#pragma mark - 事件处理
+- (void)handleCancelAction:(id)sender
+{
+    [self hideContentView];
+}
+
+#pragma mark - 系统方法重写
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    cancelButton.frame  = self.bounds;
+}
+
+#pragma mark - 设置属性
+- (void)setAnimationStatus:(CGViewAnimationStatus)animationStatus
+{
+    _animationStatus    = animationStatus;
+    if (self.disableAnimationUserInteraction) {
+        
+        BOOL userInteractionEnabled = YES;
+        if (animationStatus == CGViewAnimationStatusAniamtion) {
+            userInteractionEnabled  = NO;
+        }
+        self.userInteractionEnabled = userInteractionEnabled;
+    }
+}
+
+- (void)setDisableBackgroundButtonTools:(BOOL)disableBackgroundButtonTools
+{
+    _disableBackgroundButtonTools   = disableBackgroundButtonTools;
+    cancelButton.enabled            = disableBackgroundButtonTools;
+}
+
+@end
