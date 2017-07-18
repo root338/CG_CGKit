68a0ac660425eaa6a3c79d29d23d636b3174190f
a.chen.guang <a.chen.guang@gmail.com>
2017-07-19 00:15:59 +0800

    增加 流样式子类

---
+++ View/CustomView/CollectionView/CustomCollectionViewLayout/CGCollectionViewFloatLayout.h
@@ -0,0 +1,13 @@
+//
+//  CGCollectionViewFloatLayout.h
+//  QuickAskCommunity
+//
+//  Created by PCG on 2017/7/18.
+//  Copyright © 2017年 ym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGCollectionViewFloatLayout : UICollectionViewLayout
+
+@end
---
+++ View/CustomView/CollectionView/CustomCollectionViewLayout/CGCollectionViewFloatLayout.m
@@ -0,0 +1,13 @@
+//
+//  CGCollectionViewFloatLayout.m
+//  QuickAskCommunity
+//
+//  Created by PCG on 2017/7/18.
+//  Copyright © 2017年 ym. All rights reserved.
+//
+
+#import "CGCollectionViewFloatLayout.h"
+
+@implementation CGCollectionViewFloatLayout
+
+@end
