01d155335d9ec0f8c2cedc83fcd47e8091326d42
DY <dy@yuemei.com>
2017-08-03 18:06:00 +0800

    添加等分加载视图的基类

---
+++ CG_CGKik+Swift/UIView/UIView/BaseView/CGBaseFlowButtonsView.swift
@@ -0,0 +1,46 @@
+//
+//  CGBaseFlowButtonsView.swift
+//  QuickAskCommunity
+//
+//  Created by DY on 2017/8/3.
+//  Copyright © 2017年 ym. All rights reserved.
+//
+
+//import UIKit
+//
+//struct CGFlowButtonsConfig {
+//    
+//    var marginEdgeInsets    : UIEdgeInsets
+//    var itemSize            : CGSize
+//    var horizontalSpace     : CGFloat
+//    var verticalSpace       : CGFloat
+//    var horizontalItemCount : NSInteger
+//    
+//    init(itemSize: CGSize, marginEdgeInsets: UIEdgeInsets, horizontalSpace: CGFloat, verticalSpace: CGFloat, horizontalItemCount: NSInteger) {
+//        self.itemSize = itemSize
+//        self.marginEdgeInsets   = marginEdgeInsets
+//        self.horizontalSpace    = horizontalSpace
+//        self.verticalSpace      = verticalSpace
+//        self.horizontalItemCount    = horizontalItemCount
+//    }
+//    
+//    convenience init(maxSize: CGSize, marginEdgeInset: UIEdgeInsets, horizontalSpace: CGFloat, verticalSpace: CGFloat, horizontalItemCount: NSInteger) {
+//        
+//        
+//    }
+//}
+//
+//class CGBaseFlowButtonsView: UIView {
+//
+//    init(frame: CGRect, config: CGFlowButtonsConfig) {
+//        
+//        
+//        
+//        super.init(frame: frame)
+//        
+//        required init?(coder aDecoder: NSCoder) {
+//            fatalError("init(coder:) has not been implemented")
+//        }
+//    }
+//
+//}
