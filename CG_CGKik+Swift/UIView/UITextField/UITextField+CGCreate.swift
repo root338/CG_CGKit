aa3609e4e9c5cc4780ffe3a3be6837e92df36dae
DY <dy@yuemei.com>
2017-08-24 15:35:08 +0800

    增加UITextField的创建

--- CG_CGKik+Swift/UIView/UITextField/UITextField+CGCreate.swift
+++ CG_CGKik+Swift/UIView/UITextField/UITextField+CGCreate.swift
@@ -8,3 +8,39 @@
 
 import UIKit
 
+extension UITextField {
+    
+    convenience init(textColor: UIColor?, font: UIFont?) {
+        
+        self.init()
+        self.font       = font
+        self.textColor  = textColor
+        
+        self.clearButtonMode = .whileEditing
+    }
+    
+    convenience init(attributedPlaceholder: NSAttributedString?, textColor: UIColor?, font: UIFont?) {
+        
+        self.init(textColor: textColor, font: font)
+        self.attributedPlaceholder  = attributedPlaceholder
+    }
+    
+    convenience init(placeholder: String?, textColor: UIColor?, font: UIFont?) {
+        
+        self.init(textColor: textColor, font: font)
+        self.placeholder    = placeholder
+    }
+    
+    convenience init(text: String?, placeholder: String?, textColor: UIColor?, font: UIFont?) {
+        
+        self.init(placeholder: placeholder, textColor: textColor, font: font)
+        self.text = text
+    }
+    
+    convenience init(attributedPlaceholder: NSAttributedString?, textColor: UIColor?, font: UIFont?, backgroundColor: UIColor?) {
+        
+        self.init(attributedPlaceholder: attributedPlaceholder, textColor: textColor, font: font)
+        self.backgroundColor = backgroundColor
+    }
+    
+}
