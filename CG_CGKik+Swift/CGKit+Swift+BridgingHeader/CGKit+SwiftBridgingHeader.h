802354799906b48c2c3f59e173baaed60c1ce916
DY <dy@yuemei.com>
2017-12-20 11:12:24 +0800

    更新swift 桥接文件

--- CG_CGKik+Swift/CGKit+Swift+BridgingHeader/CGKit+SwiftBridgingHeader.h
+++ CG_CGKik+Swift/CGKit+Swift+BridgingHeader/CGKit+SwiftBridgingHeader.h
@@ -15,7 +15,13 @@
 /** 视图扩展 */
 #import "UIView+CGSetupFrame.h"
 #import "UIView+CGSetupAppearance.h"
+#import "UILabel+CGSetupAppearance.h"
+#import "UIButton+CGSetupAppearance.h"
 
+#import "UILabel+CGCreateCustomLabel.h"
+#import "UIButton+CGCreateCustomButton.h"
+
+#import "UIView+CGSearchView.h"
 #import "UIView+CGAddConstraints.h"
 
 #endif /* CGKit_SwiftBridgingHeader_h */
