81fdbc32f2c44e5183510b73bc099951da17f6c2
apple <pei.chenguang@yuemei.com>
2018-01-31 17:59:23 +0800

    设置UIControl的单项状态 ENUM

---
+++ CG_CGKik+Swift/UIView/UIControl/CGControlDefineHeader.swift
@@ -0,0 +1,46 @@
+//
+//  CGControlDefineHeader.swift
+//  QuickAskCommunity
+//
+//  Created by apple on 2018/1/30.
+//  Copyright © 2018年 ym. All rights reserved.
+//
+
+import Foundation
+
+public enum CGControlState : Int {
+    case normal
+    case highlighted
+    case disabled
+    case selected
+    case focused
+    case application
+    case reserved
+    
+    public func state() -> UIControlState {
+        
+        let state : UIControlState
+        switch self {
+        case .normal:
+            state   = .normal
+        case .highlighted:
+            state   = .highlighted
+        case .disabled:
+            state   = .disabled
+        case .selected:
+            state   = .selected
+        case .focused:
+            
+            if #available(iOS 9.0, *) {
+                state   = .focused
+            } else {
+                state   = .normal
+            }
+        case .application:
+            state   = .application
+        case .reserved:
+            state   = .reserved
+        }
+        return state
+    }
+}
