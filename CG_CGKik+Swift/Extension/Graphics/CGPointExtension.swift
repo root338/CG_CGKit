7db592f5605a5323036661f3bf76d1daf7b35d8b
apple <pei.chenguang@yuemei.com>
2018-04-28 14:51:45 +0800

    增加CGPointExtension

--- CG_CGKik+Swift/Extension/Graphics/CGPointExtension.swift
+++ CG_CGKik+Swift/Extension/Graphics/CGPointExtension.swift
@@ -8,13 +8,42 @@
 
 import Foundation
 
+enum MLOffsetType : Int {
+    case add
+    case less
+}
+
 extension CGPoint {
     
-    func cg_offset(x: CGFloat, y: CGFloat) -> CGPoint {
+    func ml_offset(x: CGFloat, y: CGFloat) -> CGPoint {
         return CGPoint.init(x: self.x + x, y: self.y + y)
     }
     
-    func cg_offset(point: CGPoint) -> CGPoint {
+    func ml_offset(point: CGPoint) -> CGPoint {
         return CGPoint.init(x: self.x + point.x, y: self.y + point.y)
+    }
+    
+    func ml_offsetLess(x: CGFloat, y: CGFloat) -> CGPoint {
+        return CGPoint.init(x: self.x - x, y: self.y - y)
+    }
+    
+    func ml_offsetLess(point: CGPoint) -> CGPoint {
+        return CGPoint.init(x: self.x - point.x, y: self.y - point.y)
+    }
+    
+    mutating func ml_mutableOffset(x: CGFloat, y: CGFloat) {
+        self = ml_offset(x: x, y: y)
+    }
+    
+    mutating func ml_mutableOffset(point: CGPoint) {
+        self = ml_offset(point: point)
+    }
+    
+    mutating func ml_mutableOffsetLess(x: CGFloat, y: CGFloat) {
+        self = ml_offsetLess(x: x, y: y)
+    }
+    
+    mutating func ml_mutableOffsetLess(point: CGPoint) {
+        self = ml_offsetLess(point: point)
     }
 }
