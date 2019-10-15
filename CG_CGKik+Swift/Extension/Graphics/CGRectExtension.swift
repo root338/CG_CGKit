8d955f0d7faa038e0f80e156727feae55c431ace
ML <1042807191@qq.com>
2019-10-15 18:58:22 +0800

    完善 CGRectExtension 方法

--- CG_CGKik+Swift/Extension/Graphics/CGRectExtension.swift
+++ CG_CGKik+Swift/Extension/Graphics/CGRectExtension.swift
@@ -10,10 +10,6 @@ import Foundation
 
 extension CGRect {
     
-    func ml_centerRect(size: CGSize) -> CGRect {
-        return CGRect.init(x: (self.size.width - size.width) / 2, y: (self.size.height - size.height) / 2, width: size.width, height: size.height)
-    }
-    
     func ml_offset(point: CGPoint) -> CGRect {
         var rect = self
         rect.origin = CGPoint.init(x: self.minX + point.x, y: self.minY + point.y)
@@ -29,7 +25,27 @@ extension CGRect {
     func ml_add(marginInsets insets: UIEdgeInsets) -> CGRect {
         return CGRect.init(x: self.minX - insets.left, y: self.minY - insets.top, width: self.width + insets.left + insets.right, height: self.height + insets.top + insets.bottom)
     }
+    
 }
+
+//MARK:- 居中计算
+extension CGRect {
+    
+    /// size 在 CGRect 中的居中区域
+    func ml_centerRect(size: CGSize) -> CGRect {
+        return CGRect.init(x: (self.size.width - size.width) / 2, y: (self.size.height - size.height) / 2, width: size.width, height: size.height)
+    }
+    
+    /// size 在 CGRect 中的水平区域，需要提供 x
+    func ml_horizontalCenterRect(size: CGSize, x: CGFloat) -> CGRect {
+        return CGRect.init(x: x, y: (self.size.height - size.height) / 2, width: size.width, height: size.height)
+    }
+    /// size 在 CGRect 中的垂直区域，需要提供 y
+    func ml_verticalCenterRect(size: CGSize, y: CGFloat) -> CGRect {
+        return CGRect.init(x: (self.size.width - size.width) / 2, y: y, width: size.width, height: size.height)
+    }
+}
+
 
 extension CGRect {
     mutating func ml_setOffset(point: CGPoint) {
