d187edb092ceab0373b78b2b5e7c98130cdd1a5e
ym <ym@yuemei.com>
2020-08-24 15:45:05 +0800

    增加链接字符串

--- Foundation/NSString/NSString+CGString.h
+++ Foundation/NSString/NSString+CGString.h
@@ -9,6 +9,23 @@
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, GMLAddStringIgnore) {
+    /// 忽略空
+    GMLAddStringIgnoreNull = 1 << 0,
+    /// 忽略首尾空白字符和换行
+    GMLAddStringIgnoreWhitespaceAndNewline = 1 << 1,
+    GMLAddStringIgnoreAll = GMLAddStringIgnoreNull | GMLAddStringIgnoreWhitespaceAndNewline,
+};
+
+@protocol GMLAddStringProtocol <NSObject>
+/// 获取的值，默认 GMLAddStringIgnoreAll
+@property (nonatomic, assign) GMLAddStringIgnore cg_ignoreValue;
+@property (nullable, nonatomic, copy) NSString * (^cg_willAppendStringHandle) (NSString *willAppendString);
+@property (nonatomic, copy, readonly) id<GMLAddStringProtocol> (^cg_appendString) (NSString *);
+- (id<GMLAddStringProtocol>)cg_appendString:(NSString *)string;
+@end
+
 @interface NSString (CGString)
 
 - (nullable NSString *)cg_stringByAppendingString:(NSString *)aString;
@@ -22,11 +39,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 /** 去除单词之间多余的空格 */
 - (nullable NSString *)cg_stringRemovalRedundantWhitespace;
+
 @end
 
 @interface NSMutableString (CGMutableString)
 
++ (NSMutableString *)cg_stringComponentsJoinedByString:(NSString *)separator usingBlock:(void(NS_NOESCAPE^) (id<GMLAddStringProtocol> mString))usingBlock;
+
 - (void)removeLastString;
+
+
 
 @end
 
--- Foundation/NSString/NSString+CGString.m
+++ Foundation/NSString/NSString+CGString.m
@@ -7,6 +7,12 @@
 //
 
 #import "NSString+CGString.h"
+#import <objc/runtime.h>
+
+@interface NSMutableString (_CGStringPrivate)<GMLAddStringProtocol>
+@property (nonatomic, strong) NSMutableArray *_gml_strings;
+- (void)_gml_clearAddStringCache;
+@end
 
 @implementation NSString (CGString)
 
@@ -52,11 +58,97 @@
 
 @implementation NSMutableString (CGMutableString)
 
++ (NSMutableString *)cg_stringComponentsJoinedByString:(NSString *)separator usingBlock:(void (NS_NOESCAPE^)(id<GMLAddStringProtocol> _Nonnull))usingBlock {
+    NSMutableString *mStr = NSMutableString.string;
+    !usingBlock?: usingBlock(mStr);
+    [mStr appendString:[mStr._gml_strings componentsJoinedByString:separator]];
+    [mStr _gml_clearAddStringCache];
+    return mStr;
+}
+
 - (void)removeLastString
 {
     if (self.length) {
         [self deleteCharactersInRange:NSMakeRange(self.length - 1, 1)];
     }
+}
+
+@end
+
+@implementation NSMutableString (GMLAddStringProtocol)
+
+#pragma mark - GMLAddStringProtocol
+- (id<GMLAddStringProtocol>)cg_appendString:(NSString *)string {
+    GMLAddStringIgnore ignore = self.cg_ignoreValue;
+    BOOL isNull = string == nil || [string isKindOfClass:NSNull.class];
+    NSString *newStr = nil;
+    if (ignore & GMLAddStringIgnoreNull) {
+        if (isNull) {
+            return self;
+        }
+    }
+    if (ignore & GMLAddStringIgnoreWhitespaceAndNewline) {
+        if (!isNull) {
+            newStr = [string stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceAndNewlineCharacterSet];
+            if (newStr.length == 0) {
+                return self;
+            }
+        }
+    }
+    if (newStr == nil) {
+        newStr = isNull ? [NSString stringWithFormat:@"%@", string] : string;
+    }
+    if (self.cg_willAppendStringHandle) {
+        newStr = self.cg_willAppendStringHandle(newStr);
+    }
+    CGDebugAssert(newStr, @"为空");
+    if (newStr == nil) {
+        return self;
+    }
+    [self._gml_strings addObject:newStr];
+    return self;
+}
+
+- (id<GMLAddStringProtocol> _Nonnull (^)(NSString * _Nonnull))cg_appendString {
+    return ^(NSString *str) {
+        return [self cg_appendString:str];
+    };
+}
+
+- (void)setCg_willAppendStringHandle:(NSString * _Nonnull (^)(NSString * _Nonnull))block {
+    objc_setAssociatedObject(self, @selector(cg_willAppendStringHandle), block, OBJC_ASSOCIATION_COPY_NONATOMIC);
+}
+
+- (NSString * _Nonnull (^)(NSString * _Nonnull))cg_willAppendStringHandle {
+    return objc_getAssociatedObject(self, _cmd);
+}
+
+- (void)_gml_clearAddStringCache {
+    objc_setAssociatedObject(self, @selector(_gml_strings), nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(self, @selector(cg_ignoreValue), nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)setCg_ignoreValue:(GMLAddStringIgnore)value {
+    objc_setAssociatedObject(self, @selector(cg_ignoreValue), @(value), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (GMLAddStringIgnore)cg_ignoreValue {
+    id obj = objc_getAssociatedObject(self, _cmd);
+    if (obj == nil) {
+        return GMLAddStringIgnoreAll;
+    }
+    return [obj integerValue];
+}
+
+- (NSMutableArray *)_gml_strings {
+    
+    NSMutableArray *mArr = objc_getAssociatedObject(self, _cmd);
+    if (mArr) {
+        return mArr;
+    }
+    mArr = NSMutableArray.array;
+    objc_setAssociatedObject(self, _cmd, mArr, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    return mArr;
 }
 
 @end
