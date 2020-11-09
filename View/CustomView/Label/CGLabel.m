f92a252819400874da8721485c5d9b40d24032ef
ym <ym@yuemei.com>
2020-11-09 18:23:25 +0800

    修改CGLabel在设置 textAlignment 时错误

--- View/CustomView/Label/CGLabel.m
+++ View/CustomView/Label/CGLabel.m
@@ -12,7 +12,11 @@
 @implementation CGLabel
 
 - (CGSize)intrinsicContentSize {
-    CGSize textSize = [self sizeThatFits:CGSizeMake(self.preferredMaxLayoutWidth, CGFLOAT_MAX)];
+    CGFloat width = self.preferredMaxLayoutWidth;
+    if (width <= 1) {
+        width = CGRectGetWidth(UIScreen.mainScreen.bounds);
+    }
+    CGSize textSize = [self sizeThatFits:CGSizeMake(width, CGFLOAT_MAX)];
     return textSize;
 }
 
@@ -20,15 +24,18 @@
     if (self.text.length == 0 || self.attributedText.string.length == 0) {
         return CGSizeZero;
     }
-    CGSize availableSize = CG_CGSizeWidthMaxSize(size, _textMarginEdgeInsets);
-    CGSize textSize = [super sizeThatFits:availableSize];
-    CGSize resultSize = CG_CGMaxSizeWidthSize(textSize, _textMarginEdgeInsets);
-    return resultSize;
+    CGSize textSize = [super sizeThatFits:size];
+    textSize = CG_CGMaxSizeWidthSize(textSize, _textMarginEdgeInsets);
+    textSize = CGSizeMake(MIN(ceil(textSize.width), size.width), MIN(ceil(textSize.height), size.height));
+    return textSize;
 }
 
 - (CGRect)textRectForBounds:(CGRect)bounds limitedToNumberOfLines:(NSInteger)numberOfLines {
     CGRect textAvailableFrame = CG_CGFrameWithMaxFrame(bounds, _textMarginEdgeInsets);
-    CGRect textRect = [super textRectForBounds:textAvailableFrame limitedToNumberOfLines:numberOfLines];
+    CGRect textBounds = textAvailableFrame;
+    textBounds.origin.x = 0;
+    CGRect textRect = [super textRectForBounds:textBounds limitedToNumberOfLines:numberOfLines];
+    textRect.origin.x += _textMarginEdgeInsets.left;
     switch (self.textVerticalAlignment) {
         case CGLabelTextVerticalAlignmentCenter:
             textRect.origin.y += (CGRectGetHeight(textAvailableFrame) - CGRectGetHeight(textRect)) / 2;
@@ -50,4 +57,14 @@
     [super drawTextInRect:textRect];
 }
 
+- (void)setBounds:(CGRect)bounds {
+    [super setBounds:bounds];
+    CGFloat width = CGRectGetWidth(bounds);//CG_CGWidthWithMaxWidth(CGRectGetWidth(bounds), _textMarginEdgeInsets);
+    if (self.preferredMaxLayoutWidth != width) {
+        self.preferredMaxLayoutWidth = width;
+    }
+}
+
 @end
+
+
