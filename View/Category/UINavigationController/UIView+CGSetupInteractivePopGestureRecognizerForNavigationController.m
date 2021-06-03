a8c60e65e46106490e3741ae8bdece7b2de28e67
ym <ym@yuemei.com>
2021-06-03 13:54:09 +0800

    删除无用文件

--- View/Category/UINavigationController/UIView+CGSetupInteractivePopGestureRecognizerForNavigationController.m
+++ View/Category/UINavigationController/UIView+CGSetupInteractivePopGestureRecognizerForNavigationController.m
@@ -9,9 +9,10 @@
 #import "UIView+CGSetupInteractivePopGestureRecognizerForNavigationController.h"
 
 #import "UIView+CGSearchView.h"
-
-#import "CGNavigationController.h"
-
+// 注释原因：这个宏和项目是否添加该文件无关，只要在项目实体文件夹中就会通过
+//#if __has_include("CGNavigationController.h")
+//#import "CGNavigationController.h"
+//#endif
 @implementation UIView (CGSetupInteractivePopGestureRecognizerForNavigationController)
 
 - (nullable UINavigationController *)getViewNavigationController
@@ -23,14 +24,17 @@
 {
     BOOL beforePopGestureRecognizerEnable           = NO;
     UINavigationController *navigationController    = [self getViewNavigationController];
-    if ([navigationController isKindOfClass:[CGNavigationController class]]) {
-        
-        CGNavigationController *navi        = (id)navigationController;
-        beforePopGestureRecognizerEnable    = navi.enablePopGestureRecognizer;
-        if (navi.enablePopGestureRecognizer != enable) {
-            navi.enablePopGestureRecognizer = enable;
-        }
-    }else {
+//#if __has_include("CGNavigationController.h")
+//    if ([navigationController isKindOfClass:[CGNavigationController class]]) {
+//
+//        CGNavigationController *navi        = (id)navigationController;
+//        beforePopGestureRecognizerEnable    = navi.enablePopGestureRecognizer;
+//        if (navi.enablePopGestureRecognizer != enable) {
+//            navi.enablePopGestureRecognizer = enable;
+//        }
+//    }else
+//#endif
+    {
         
         beforePopGestureRecognizerEnable    = navigationController.interactivePopGestureRecognizer.enabled;
         if (navigationController.interactivePopGestureRecognizer.enabled != enable) {
